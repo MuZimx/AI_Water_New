ee3f4732af48cc7a681a47276244de1d6cd164c2
lvshujun <Lvshujun0918@163.com>
2026-03-17 14:46:56 +0800

    feat(mobile): 适配返回键

--- frontend/android/app/src/main/java/com/aiwater/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/aiwater/app/MainActivity.java
@@ -1,5 +1,23 @@
 package com.aiwater.app;
 
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (bridge != null && bridge.getWebView() != null && bridge.getWebView().canGoBack()) {
+			bridge.getWebView().goBack();
+			return;
+		}
+
+		super.onBackPressed();
+	}
+}
